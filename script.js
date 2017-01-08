1324dd41b0d8bf9a0728adb3c430761806d570dc
suchaone <invisible.chinchilla@gmail.com>
2017-01-08 01:03:57 -0500

    my blog

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".footnote-link").click(function(e) {
+    e.preventDefault();
+    var active = $(this).hasClass("active");
+    $(".footnote-link").removeClass("active");
+    $(".footnote-expanded").hide();
+    if (!active) {
+      $($(this).attr("href")).fadeIn();
+      $(this).addClass("active");
+    }
+  });
+});
